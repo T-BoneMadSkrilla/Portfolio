e75d9782b89d549e48f4f212a52f5356013d2abe
GIT SERVICE NAME (NOT USERNAME) <GIT SERVICE EMAIL>
2019-01-17 00:12:43 -0600

    saving to github for safety

--- portfolio/src/components/landingpage/LandingPage.js
+++ portfolio/src/components/landingpage/LandingPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './LandingPage.css';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import About from '../about/About';
+import Work_projects from '../work_projects/Work_projects';
 
 class LandingPage extends Component {
     render(){
@@ -28,12 +29,12 @@ class LandingPage extends Component {
                         <div className="landingPageText">
                             <div className="intro"> Hi, I'm Tyler </div>
                             
-                            <div> I'm an ethusastic web developer </div>
+                            <div> I'm an ethusastic web developer with an equal balence between being technical and creative</div>
                         </div>
                      </ReactCSSTransitionGroup>
                      </div>
 
-
+                    <div className="about">
                      <ReactCSSTransitionGroup
                         transitionName="fadeIn"
                         transitionAppear={true}
@@ -42,10 +43,26 @@ class LandingPage extends Component {
                         transitionLeave={true}>                       
                       <div className="firstDivider"></div>
                      </ReactCSSTransitionGroup>
-
                      <div>
                     <About/>
                      </div>
+                        </div>
+
+
+                     <div className="workProjects">
+                     <ReactCSSTransitionGroup
+                        transitionName="fadeIn"
+                        transitionAppear={true}
+                        transitionAppearTimeout={19000}
+                        transitionEnter={false}
+                        transitionLeave={true}>                       
+                      <div className="secondDivider"></div>
+                     </ReactCSSTransitionGroup>
+
+                     <div>
+                        <Work_projects/>
+                     </div>
+                        </div>
             </div>
         )
     }
